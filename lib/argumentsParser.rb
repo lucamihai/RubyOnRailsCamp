b919556980817a09d196ea5126dcda8673c1f1dd
Mihai Luca <luca.mihai97@yahoo.com>
2022-03-26 13:04:43 +0200

    Use 2 spaces instead of 4

--- lib/argumentsParser.rb
+++ lib/argumentsParser.rb
@@ -1,15 +1,15 @@
 require "./lib/arguments.rb"
 
 class ArgumentsParser
-    def self.parse()
-        arguments = Arguments.new
-        arguments.searchParam = getSearchParam
+  def self.parse()
+    arguments = Arguments.new
+    arguments.searchParam = getSearchParam
 
-        return arguments
-    end
+    return arguments
+  end
 
-    def self.getSearchParam()
-        # something broke, so out of laziness I just do this
-        return ARGV[1]
-    end
+  def self.getSearchParam()
+    # something broke, so out of laziness I just do this
+    return ARGV[1]
+  end
 end
