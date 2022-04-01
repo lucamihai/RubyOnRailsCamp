31c41658b246a880fc261bf2af195f4fe2bfbf8b
Mihai Luca <luca.mihai97@yahoo.com>
2022-04-01 14:05:29 +0300

    Use subject in ArgumentParser tests

--- spec/lib/argument_parser_spec.rb
+++ spec/lib/argument_parser_spec.rb
@@ -4,26 +4,29 @@ require "./lib/arguments_parser.rb"
 RSpec.describe ArgumentsParser do
   describe ".parse" do
     context "Given show command" do
+      subject { ArgumentsParser.parse(["show", "faraday"]) }
       it "returns expected entity" do
-        expect(ArgumentsParser.parse(["show", "faraday"]).name).to eq "faraday"
-        expect(ArgumentsParser.parse(["show", "faraday"]).search_term).to eq ""
-        expect(ArgumentsParser.parse(["show", "faraday"]).search_limit).to eq 0
+        expect(subject.name).to eq "faraday"
+        expect(subject.search_term).to eq ""
+        expect(subject.search_limit).to eq 0
       end
     end
 
     context "Given search command" do
+      subject { ArgumentsParser.parse(["search", "faraday"]) }
       it "returns expected entity" do
-        expect(ArgumentsParser.parse(["search", "faraday"]).name).to eq ""
-        expect(ArgumentsParser.parse(["search", "faraday"]).search_term).to eq "faraday"
-        expect(ArgumentsParser.parse(["search", "faraday"]).search_limit).to eq 0
+        expect(subject.name).to eq ""
+        expect(subject.search_term).to eq "faraday"
+        expect(subject.search_limit).to eq 0
       end
     end
 
     context "Given search command with limit" do
+      subject { ArgumentsParser.parse(["search", "faraday", "--limit", "3"]) }
       it "returns expected entity" do
-        expect(ArgumentsParser.parse(["search", "faraday", "--limit", "3"]).name).to eq ""
-        expect(ArgumentsParser.parse(["search", "faraday", "--limit", "3"]).search_term).to eq "faraday"
-        expect(ArgumentsParser.parse(["search", "faraday", "--limit", "3"]).search_limit).to eq 3
+        expect(subject.name).to eq ""
+        expect(subject.search_term).to eq "faraday"
+        expect(subject.search_limit).to eq 3
       end
     end
   end
