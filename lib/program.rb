55ba8da66c998e33500c9339d166d633cfe808fa
Mihai Luca <luca.mihai97@yahoo.com>
2022-04-02 13:05:45 +0300

    Prettify output

--- lib/program.rb
+++ lib/program.rb
@@ -13,7 +13,10 @@ class Program
     if (arguments.name != '')
       gem_info = @ruby_gems_api.gem_by_name(arguments.name)
       display_gem_info(gem_info)
-    elsif (arguments.search_term != '')
+      puts "---------------------------------\n"
+    end
+    
+    if (arguments.search_term != '')
       gems_info = @ruby_gems_api.search_gems(arguments.search_term, arguments.search_limit)
       display_gems_info(gems_info)
     end
@@ -22,17 +25,20 @@ class Program
   private
 
   def display_gem_info(gem_info)
+    puts "---------------------------------\n"
     info = gem_info.info.length > 100 ? "#{gem_info.info[0, 100]}..." : gem_info.info;
 
     puts "#{gem_info.name}\n"
-    puts "#{info}\n\n"
+    puts "#{info}\n"
   end
 
   def display_gems_info(gems_info)
-    puts "Found #{gems_info.count} gems"
+    puts "---------------------------------\n"
+    puts "Found #{gems_info.count} gems\n"
     for gem_info in gems_info
       display_gem_info(gem_info)
     end
+    puts "---------------------------------\n"
   end
 
 end
