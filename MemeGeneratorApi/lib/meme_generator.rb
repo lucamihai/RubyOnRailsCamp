553fd813340e6f4c28e098fd86a24b0eb2fc0fc6
Mihai Luca <luca.mihai97@yahoo.com>
2022-04-28 16:21:38 +0300

    Add support for images from URL

--- MemeGeneratorApi/lib/meme_generator.rb
+++ MemeGeneratorApi/lib/meme_generator.rb
@@ -1,4 +1,5 @@
 require 'mini_magick'
+require "open-uri"
 require './lib/arguments'
 
 module MemeGenerator
@@ -7,7 +8,19 @@ module MemeGenerator
       raise 'You need to provide at least original_image_path, final_image_path and text in order to generate some spicy memes'
     end
 
-    image = MiniMagick::Image.open(arguments.original_image_path)
+    original_image_path = arguments.original_image_path
+    temp_image_path = nil
+
+    if (is_url(arguments.original_image_path))
+      temp_image_path = Time.now.to_i.to_s
+      URI.open(arguments.original_image_path.chomp) do |image|
+        File.open(temp_image_path, "wb") do |file|
+          file.write(image.read)
+        end
+      end
+    end
+
+    image = MiniMagick::Image.open(original_image_path)
     image.combine_options do |c|
       c.gravity(arguments.gravity)
       c.stroke(arguments.under_color)
@@ -18,7 +31,17 @@ module MemeGenerator
       c.font arguments.font
     end
 
+    if (temp_image_path != nil)
+      File.delete(temp_image_path)
+    end
+
     image.write(arguments.final_image_path)
     File.expand_path(arguments.final_image_path)
+  end
+
+  private
+
+  def self.is_url(path)
+    path.include?('http')
   end
 end
