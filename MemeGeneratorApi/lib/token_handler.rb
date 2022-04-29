abd325116660206bfd50824a67078ec8e150b456
Mihai Luca <luca.mihai97@yahoo.com>
2022-04-29 13:49:33 +0300

    Implement TokenHandler

--- MemeGeneratorApi/lib/token_handler.rb
+++ MemeGeneratorApi/lib/token_handler.rb
@@ -1,11 +1,22 @@
+require 'jwt'
+require './lib/user_repository'
+
 class TokenHandler
+  def initialize(user_repository = UserRepository.new)
+    @user_repository = user_repository
+  end
+
   def generate_token(username)
-    # TODO
-    "some authentic token"
+    payload = { username: username }
+
+    # I know, not very secure
+    token = JWT.encode(payload, nil, 'none')
   end
 
   def is_valid(token)
-    # TODO
-    true
+    # I know, not very secure
+    decoded_token = JWT.decode(token, nil, false)
+    username = decoded_token[0]['username']
+    @user_repository.user_exists(username)
   end
 end
